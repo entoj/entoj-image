10efd6cb5cf88e07a8ed87b663d4c61c126ffb99
Christian Auth <c.auth@syzygy.de>
2017-12-19 13:31:32 +0100

    feat: adds module registration

--- source/index.js
+++ source/index.js
@@ -1,8 +1,40 @@
 /**
- *
+ * Registers with default configurations
+ */
+function register(configuration, options)
+{
+    // Nunjucks filter
+    configuration.mappings.add(require('entoj-system').nunjucks.Environment,
+        {
+            '!filters':
+            [
+                require('./nunjucks/index.js').filter.ImageUrlFilter
+            ]
+        }
+    );
+
+    // Routes
+    configuration.commands.add(require('entoj-system').command.ServerCommand,
+        {
+            options:
+            {
+                routes:
+                [
+                    {
+                        type: require('./server/index.js').route.ImageRoute
+                    }
+                ]
+            }
+        });
+}
+
+
+/**
+ * API
  */
 module.exports =
 {
+    register: register,
     configuration: require('./configuration/index.js'),
     nunjucks: require('./nunjucks/index.js'),
     renderer: require('./renderer/index.js'),
